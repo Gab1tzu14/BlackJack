6d79a387cfc1372f47df1a0ba41c823e9c162610
Gab1tzu14 <gabrielm200.0001@Gmail.com>
2024-08-07 01:52:18 +0300

    asd

---
+++ deposit.js
@@ -0,0 +1,8 @@
+
+setTimeout(money => {
+    alert("Just a joke, but I would like some $$$$$$$$$")
+    window.location.href = "Game.html";
+}, 5000);
+
+function nopay(){
+    alert("You can't pay :(")}
